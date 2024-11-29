bf444265116c362a0c0bd82840ae9540490f81ab
Tharusha-sandipa <tharuandayoutu723@gmail.com>
2024-11-29 14:30:00 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "enter your session",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/MPszfDn/4cfd38815fe144de.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M THARUSHA-MD WHATSAPP BOT 😊♻️",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",
+PREFIX: process.env.PREFIX || ".",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+AUTO_REACT: process.env.AUTO_REACT || "true",
+REJECT_CALL: process.env.REJECT_CALL || "true",
+HEART_REACT: process.env.HEART_REACT || "true",
+OWNER_REACT: process.env.OWNER_REACT || "true",
+BOT_NAME: process.env.BOT_NAME || "SANDIPA-MD",
+};
